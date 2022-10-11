6aad828c488948e36cbd8b0b99ebd01a67fa0c5b
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-10-11 14:08:17 +0200

    fix mouse animation for mobile

--- src/components/MouseStars.js
+++ src/components/MouseStars.js
@@ -8,7 +8,6 @@ const MouseStars = () => {
 
   /* HANDLE FUNCTIONS */
   const handleMouseStars = (event) => {
-    event.preventDefault();
 
     const stars = document.createElement('span');
     stars.classList.add('stars');
@@ -32,10 +31,34 @@ const MouseStars = () => {
   };
 
 
+  const handleMobileStars = (event) => {
+
+    const stars = document.createElement('span');
+    stars.classList.add('stars');
+    body.appendChild(stars);
+
+    const x = event.touches[0].clientX;
+    const y = event.touches[0].clientY;
+
+    const size = Math.random() * 30;
+    const transformValue = Math.random() * 360;
+
+    stars.style.left = x + 'px';
+    stars.style.top = y + 'px';
+    stars.style.width = 2 + size + 'px';
+    stars.style.height = 2 + size + 'px';
+    stars.style.transform = `rotate(${transformValue}deg)`;
+
+    setTimeout(() => {
+      stars.remove();
+    }, 1000);
+  };
+
+
   /* LISTENERS */
   const mouseListener = () => document.addEventListener('mousemove', handleMouseStars);
   mouseListener();
-  const mobileTouchListener = () => document.addEventListener('touchmove', handleMouseStars);
+  const mobileTouchListener = () => document.addEventListener('touchmove', handleMobileStars);
   mobileTouchListener();
 
 };
